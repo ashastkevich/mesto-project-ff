37af65d86d3c09894eee1c63577c61735a299f51
ashastkevich <ashastkevich@gmail.com>
2025-05-03 15:39:40 +0300

    Add and remove cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,23 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
+function addCard(name, link) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  cardElement.querySelector('.card__image').src = link;
+  cardElement.querySelector('.card__image').alt = name;
+  cardElement.querySelector('.card__title').textContent = name;
+  cardElement.querySelector('.card__delete-button').addEventListener('click', removeCard);
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function removeCard(evt) {
+  evt.target.closest('.card').remove();
+}
+
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(item => document.querySelector('.places__list').append(addCard(item.name, item.link)));
